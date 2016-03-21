85292bdf240f1b74cc7b92ed5cacfad32cbcc08a
Richard Giddings <rgiddings20@hotmail.com>
2016-03-21 16:09:28 +0000

    Corrected code in response to a warning about deprecated functioanlity in Django 1.10

--- photosite/urls.py
+++ photosite/urls.py
@@ -3,20 +3,20 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from . import views
-
+import django.views.static
 
 app_name = 'photosite'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^cat_filter/$', views.cat_filter, name='cat_filter'),
     url(r'^photo_search/$', views.photo_search, name='photo_search'),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+    url(r'^media/(?P<path>.*)$', django.views.static.serve, {
             'document_root': settings.MEDIA_ROOT,
         }),
-    url(r'^cat_filter/media/(?P<path>.*)$', 'django.views.static.serve', {
+    url(r'^cat_filter/media/(?P<path>.*)$', django.views.static.serve, {
             'document_root': settings.MEDIA_ROOT,
         }),
-    url(r'^photo_search/media/(?P<path>.*)$', 'django.views.static.serve', {
+    url(r'^photo_search/media/(?P<path>.*)$', django.views.static.serve, {
             'document_root': settings.MEDIA_ROOT,
         }),
 ]
